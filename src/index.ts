5716949ad3a919e0108679034ec7912f46969fcf
The-Doctor-What <hitv666@gmail.com>
2023-09-24 11:30:05 +0300

    Use of import and export: Replaced imports with ES6 syntax for managing dependencies and exporting functions.
    Error Handling: Added error handling for better robustness of the application against potential issues when reading files and moving them to other directories. Errors are logged with detailed messages.
    
    Parallel File Processing: Instead of using await in a for loop, files are processed in parallel using Promise.all, which can improve performance.
    
    Centralized File Extensions Storage: File extensions for each category are now stored in the extensionsToCategories object, making the code more maintainable and readable.
    
    Enhanced Logging: Logging has been improved to provide more informative messages about errors and the progress of the program.

--- src/index.ts
+++ src/index.ts
@@ -2,40 +2,53 @@ import fs from "fs";
 import 'dotenv/config.js'
 
 async function teleport(extensions: string[], path: string, file: any) {
-    const folder = process.env.FOLDER
-    for (const extension of extensions) {
-        if (file.endsWith(extension)) {
-            if (!fs.existsSync(`${folder}/${path}`)) fs.mkdirSync(`${folder}/${path}`)
-            fs.renameSync(`${folder}/${file}`, `${folder}/${path}/${file}`)
-            console.log(`Logs » Файл "${file}" перемещен в папку ${path}`)
+    try {
+        const folder = process.env.FOLDER;
+        for (const extension of extensions) {
+            if (file.endsWith(extension)) {
+                if (!fs.existsSync(`${folder}/${path}`)) fs.mkdirSync(`${folder}/${path}`);
+                fs.renameSync(`${folder}/${file}`, `${folder}/${path}/${file}`);
+                console.log(`Logs » Файл "${file}" перемещен в папку ${path}`);
+            }
         }
+    } catch (error) {
+        console.error(`Logs » Ошибка при перемещении файла "${file}": ${error.message}`);
     }
 }
 
 async function main() {
-    console.log(`Logs » Запуск проекта...`)
+    try {
+        console.log(`Logs » Запуск проекта...`);
 
-    const path = process.env.FOLDER
-    if (!path) return console.log(`Logs » Путь не указан`)
-    console.log(`Logs » Путь: ${path}`)
+        const path = process.env.FOLDER;
+        if (!path) return console.log(`Logs » Путь не указан`);
+        console.log(`Logs » Путь: ${path}`);
 
-    const fs = require('fs')
-    const files = fs.readdirSync(path)
-    if (!files) return console.log(`Logs » Файлы не найдены`)
+        const files = fs.readdirSync(path);
+        if (!files) return console.log(`Logs » Файлы не найдены`);
 
-    for (const file of files) {
+        const extensionsToCategories = {
+            images: ['.jpg', '.png', '.jpeg', '.gif', '.svg'],
+            videos: ['.mp4', '.avi', '.mov'],
+            audios: ['.mp3', '.wav', '.ogg'],
+            documents: ['.doc', '.docx', '.txt', '.pdf', '.xls', '.xlsx', '.ppt', '.pptx', '.odt'],
+            archives: ['.zip', '.rar', '.7z', '.tar'],
+            programs: ['.exe', '.msi', '.app'],
+            torrents: ['.torrent', '.ac3'],
+        };
 
-        await teleport(['.jpg', '.png', '.jpeg', '.gif', '.svg'], 'images', file)
-        await teleport(['.mp4', '.avi', '.mov'], 'videos', file)
-        await teleport(['.mp3', '.wav', '.ogg'], 'audios', file)
-        await teleport(['.doc', '.docx', '.txt', '.pdf', '.xls', '.xlsx', '.ppt', '.pptx', '.odt'], 'documents', file)
-        await teleport(['.zip', '.rar', '.7z', '.tar'], 'archives', file)
-        await teleport(['.exe', '.msi', '.app'], 'programs', file)
-        await teleport(['.torrent', '.ac3'], 'torrents', file)
+        const promises = files.map(async (file) => {
+            for (const [category, extensions] of Object.entries(extensionsToCategories)) {
+                await teleport(extensions, category, file);
+            }
+        });
 
+        await Promise.all(promises);
+
+        console.log(`Logs » Завершение работы...`);
+    } catch (error) {
+        console.error(`Logs » Критическая ошибка: ${error.message}`);
     }
-
-    console.log(`Logs » Завершение работы...`)
 }
 
-main().then()
+main().then();
